bb9288d160daa9b35b4693a57583c26e01cf4696
coderaiser <coderaiser@cloudcmd.io>
2016-11-16 15:01:13 +0200

    chore(flop) rm iife

--- bin/flop.js
+++ bin/flop.js
@@ -1,75 +1,72 @@
 #!/usr/bin/env node
 
-var minimist    = require('minimist'),
-    flop        = require('..'),
-    argv        = minimist(process.argv.slice(2));
+'use strict';
 
-(function(){
-    'use strict';
-    
-    var path,
-        keys    = Object.keys(argv);
-    
-    if (keys.length === 1)
-        if (argv._.length)
-            unknown();
-        else
-            help();
+var minimist = require('minimist');
+var flop = require('..');
+var argv = minimist(process.argv.slice(2));
+
+var path;
+var keys = Object.keys(argv);
+
+if (keys.length === 1)
+    if (argv._.length)
+        unknown();
     else
-        keys.some(function(name) {
-            var isDone = true;
-            
-            if (name === 'h' || name === '')
-                name = 'help';
-            
-            switch(name) {
-            case 'help':
-                help();
-                break;
-            
-            case 'read':
-                path = argv.read;
-                break;
-            
-            case 'r':
-                path = argv.r;
-                break;
-            
-            case '_':
-                isDone = false;
-                break;
-            
-            default:
-                isDone = true;
-                unknown();
-            }
-            
-            return isDone;
-        });
-    
-    if (path) {
-        flop.read(path, function(error, data) {
-            if (error)
-                console.log(error);
-            else
-                console.log(data);
-        });
-    }
-    
-    function help() {
-        var message =   'flop - folder operations module.\n'        +
-                        'options: \n'                               +
-                        '-h, --help - show this message\n'          +
-                        '-r, --read - get directory content\n';
+        help();
+else
+    keys.some(function(name) {
+        var isDone = true;
         
-        console.log(message);
-    }
-    
-    function unknown() {
-        var message =   'Unknown parameter. For help use: flop -h';
+        if (name === 'h' || name === '')
+            name = 'help';
         
-        console.log(message);
-    }
+        switch(name) {
+        case 'help':
+            help();
+            break;
+        
+        case 'read':
+            path = argv.read;
+            break;
+        
+        case 'r':
+            path = argv.r;
+            break;
+        
+        case '_':
+            isDone = false;
+            break;
+        
+        default:
+            isDone = true;
+            unknown();
+        }
+        
+        return isDone;
+    });
+
+if (path) {
+    flop.read(path, function(error, data) {
+        if (error)
+            console.log(error);
+        else
+            console.log(data);
+    });
+}
+
+function help() {
+    var message =   'flop - folder operations module.\n'        +
+                    'options: \n'                               +
+                    '-h, --help - show this message\n'          +
+                    '-r, --read - get directory content\n';
     
-})();
+    console.log(message);
+}
+
+function unknown() {
+    var message =   'Unknown parameter. For help use: flop -h';
+    
+    console.log(message);
+}
 
