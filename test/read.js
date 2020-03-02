7b86bbec09937c12e9cbbf71e3bbeb1e3fd3563e
coderaiser <coderaiser@cloudcmd.io>
2020-03-02 18:54:16 +0200

    fix(flop) remove

--- test/read.js
+++ test/read.js
@@ -14,70 +14,68 @@ const fixture = path.join(__dirname, 'fixture');
 const empty = path.join(fixture, 'empty');
 mkdirp.sync(empty);
 
-test('flop: read: size', (t) => {
-    flop.read(empty, 'size', (e, size) => {
-        t.equal(size, '0b', 'should size to be equal');
-        t.end();
-    });
+test('flop: read: size', async (t) => {
+    const size = await flop.read(empty, 'size');
+    
+    t.equal(size, '0b', 'should size to be equal');
+    t.end();
 });
 
-test('flop: read: size raw', (t) => {
-    flop.read(empty, 'size raw', (e, size) => {
-        t.equal(size, 0, 'should size to be equal');
-        t.end();
-    });
+test('flop: read: size raw', async (t) => {
+    const size = await flop.read(empty, 'size raw');
+    
+    t.equal(size, 0, 'should size to be equal');
+    t.end();
 });
 
-test('flop: read: time', (t) => {
-    flop.read(empty, 'time', (e, time) => {
-        t.ok(time instanceof Date, 'should retun date');
-        t.end();
-    });
+test('flop: read: time', async (t) => {
+    const time = await flop.read(empty, 'time');
+    
+    t.ok(time instanceof Date, 'should retun date');
+    t.end();
 });
 
-test('flop: read: time raw', (t) => {
-    flop.read(empty, 'time raw', (e, time) => {
-        t.notOk(Number.isNaN(time), 'should retun number');
-        t.end();
-    });
+test('flop: read: time raw', async (t) => {
+    const time = await flop.read(empty, 'time raw');
+    t.notOk(Number.isNaN(time), 'should retun number');
+    t.end();
 });
 
-test('flop: read: raw', (t) => {
-    flop.read(empty, 'raw', (e, result) => {
-        const expect = {
-            path: empty + path.sep,
-            files: [],
-        };
-        t.deepEqual(result, expect, 'should return result');
-        t.end();
-    });
+test('flop: read: raw', async (t) => {
+    const result = await flop.read(empty, 'raw');
+    const expect = {
+        path: empty + path.sep,
+        files: [],
+    };
+    
+    t.deepEqual(result, expect, 'should return result');
+    t.end();
 });
 
-test('flop: read', (t) => {
-    flop.read(empty, (e, result) => {
-        const expect = {
-            path: empty + path.sep,
-            files: [],
-        };
-        t.deepEqual(result, expect, 'should return result');
-        t.end();
-    });
+test('flop: read', async (t) => {
+    const result = await flop.read(empty);
+    const expect = {
+        path: empty + path.sep,
+        files: [],
+    };
+    
+    t.deepEqual(result, expect, 'should return result');
+    t.end();
 });
 
-test('flop: read: options', (t) => {
+test('flop: read: options', async (t) => {
     const readify = stub()
         .returns(Promise.resolve());
     
     mockRequire('readify', readify);
     const flop = reRequire('..');
     
-    const callback = stub();
     const options = {
         order: 'asc',
         sort: 'name',
     };
     
-    flop.read(empty, options, callback);
+    await flop.read(empty, options);
     
     const expect = [
         empty,
